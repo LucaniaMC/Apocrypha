a763832aa718e587d1b25e574896f8a53e0e1524
LucaniaMC <yangr2002@gmail.com>
2025-01-11 15:33:06 -0600

    Sludge Damage + Minor Adjustments
    Made the sludge enemy damagable.
    
    Changed the Z depth of attack spark effects on the dummy enemy so they are always displayed in front.
    
    Adjusted the lifetime of jump and landing particles.
    
    Rearranged objects in test scene.

--- Assets/Scripts/Sludge.cs
+++ Assets/Scripts/Sludge.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class Sludge : MonoBehaviour
 {
+
+    public GameObject sprite;
+    public GameObject spark;
+
+
     void OnTriggerEnter2D(Collider2D other) //Same as player attack, maybe they can share the same script?
     {
         if (other.gameObject.layer == 3) 
         {
             other.gameObject.GetComponent<HealthSystem>().Damage(10);
         }
+    }
+
+    public void onDamage()
+    {
+        Instantiate(spark, new Vector3(sprite.transform.position.x , sprite.transform.position.y , -2), Quaternion.identity); //-2 on Z axis to put the effect in front
+    }
+
+
+    public void Death()
+    {
+        Destroy(gameObject);
     }
 }
